b331a81fc381b51d3a0156cce6ac212ed9407e0e
RandomPhile <richifurlan@gmail.com>
2021-10-27 13:50:54 +0200

    esp5

--- esp5/esp5.py
+++ esp5/esp5.py
@@ -4,8 +4,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 colori = ['b','g','r','c','m','y','k']
 
-R = 100
+'''
+Saw : 1/100 Hz, 3Vpp
 
+
+'''
+
+R = 100
 
 def I_func(V, R_L, alpha):
 	return (V * 1e3)/(R_L * (1 + alpha * V**2))
@@ -17,17 +22,11 @@ I = np.array([1,3,3,4,5,6,7,8,9])*2
 plt.plot(V, I, 'b-', label='data')
 
 
-#Senza intervallo di R_L, alpha
-popt, pcov = curve_fit(I_func, V, I)
+popt, pcov = curve_fit(I_func, V, I)#bounds=([], [])
 plt.plot(V, I_func(V, *popt), 'r-', label='fit: R_L=%5.3f, alpha=%5.3f' % tuple(popt))
-
-#Con intervallo
-popt, pcov = curve_fit(I_func, V, I, bounds=([400,-1], [450, 1]))
-plt.plot(V, I_func(V, *popt), 'g--', label='fit: R_L=%5.3f, alpha=%5.3f' % tuple(popt))
-
 
 
 plt.xlabel('V')
 plt.ylabel('I')
 plt.legend()
-plt.show()
+#plt.show()
