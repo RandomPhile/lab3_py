e0ee21dba8e6d2c62842c79aee8209af0b36138b
RandomPhile <richifurlan@gmail.com>
2021-10-27 15:15:51 +0200

    salvaaa

--- esp5/esp5.py
+++ esp5/esp5.py
@@ -1,3 +1,4 @@
+import csv
 from scipy.optimize import curve_fit
 from math import *
 import numpy as np
@@ -13,11 +14,28 @@ Saw : 1/100 Hz, 3Vpp
 R = 100
 
 def I_func(V, R_L, alpha):
-	return (V * 1e3)/(R_L * (1 + alpha * V**2))
+	return (V * 10**3)/(R_L * (1 + alpha * V**2))
 
-V = np.array([1,2,3,4,5,6,7,8,9])
 
-I = np.array([1,3,3,4,5,6,7,8,9])*2
+
+with open('parte1/scope_1.csv', newline='') as csvfile:
+	V1 = np.array([])
+	V2 = np.array([])
+	reader = csv.DictReader(csvfile)
+
+	i = 0
+	for row in reader:
+		#if i>25 and i<280:
+		if i>290 and i<600:
+			V1 = np.append(V1, float(row['1']))
+			V2 = np.append(V2, float(row['2']))
+		i+=1
+
+V1 = V1[2:]
+V2 = V2[2:]
+
+I = (V1/R)*10**3
+V = V2
 
 plt.plot(V, I, 'b-', label='data')
 
@@ -29,4 +47,4 @@ plt.plot(V, I_func(V, *popt), 'r-', label='fit: R_L=%5.3f, alpha=%5.3f' % tuple(
 plt.xlabel('V')
 plt.ylabel('I')
 plt.legend()
-#plt.show()
+plt.show()
