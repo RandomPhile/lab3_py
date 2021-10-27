21c06093f1ad39ef3ab6ccfc01ca11e01cdcf65b
RandomPhile <richifurlan@gmail.com>
2021-10-27 17:21:38 +0200

    fine

--- esp5/esp5_3.py
+++ esp5/esp5_3.py
@@ -5,16 +5,26 @@ import numpy as np
 import matplotlib.pyplot as plt
 colori = ['b','g','r','c','m','y','k']
 
-'''
-l'onda quadra scende in 26.2 microsecondi
-periodo 22ms
 
-'''
-R1 = np.array([10e3, 1e3 ])
-R2 = np.array([10e3, 10e3])
+R1 = np.array([10e3, 1e3 , 10e3])
+R2 = np.array([10e3, 10e3, 1e3 ])
 
 rapporto = R1/R2
+print(rapporto)
 
-T   = np.array([22.1, ])*1e-3
+t_discesa = np.array([26.2, 28.9, 29])*1e-6
+
+T   = np.array([22.1, 3.81, 61.1])*1e-3
 f = 2*pi/T
-Vpp = np.array([21.4, 21.19])
+Vpp = np.array([21.4, 21.19, 20.94])
+
+fig, ax0 = plt.subplots()
+ax0.plot(rapporto, Vpp, 'b*', label='ampiezza')
+ax1 = ax0.twinx()
+
+ax1.plot(rapporto, T, 'r*', label='periodo')
+
+ax1.legend()
+
+print(log(21))
+#plt.show()
