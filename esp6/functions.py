27fabfc4938491a1a2383f57381a877d540087e1
RandomPhile <richifurlan@gmail.com>
2021-11-03 20:42:28 +0100

    fun

--- esp6/functions.py
+++ esp6/functions.py
@@ -21,3 +21,30 @@ def data_from_csv(name_csv,two_channels=True):
 		return t,V1,V2
 	else:
 		return t,V1
+
+def k_tr(t_,T):
+	out = np.array([])
+	for t in t_:
+		if abs(t) < T:
+			out = np.append(out, 1-abs(t)/T)
+		else:
+			out = np.append(out, 0)
+	return out
+
+def r(t,t_pt,V_pt,T):
+	out = 0
+	for j in range(len(t_pt)):
+		out += -V_pt[j] * k_tr(t + t_pt[j],T)
+	return out
+
+def reconstruct(t,V,T,delay):
+	offset = 3
+	t_pt = np.array([])
+	V_pt = np.array([])
+
+	for j in range(offset, len(t)-offset):
+		if j%40==0:
+			t_pt = np.append(t_pt, t[j] - delay)
+			V_pt = np.append(V_pt, V[j])
+
+	return t_pt, V_pt, r(t,t_pt,V_pt,T)
