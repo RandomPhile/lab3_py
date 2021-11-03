a085b28ddd18d081aadccddc8379b6af75aced76
RandomPhile <richifurlan@gmail.com>
2021-11-03 23:59:14 +0100

    sposto funzioni

--- esp6/functions.py
+++ esp6/functions.py
@@ -1,5 +1,6 @@
 import numpy as np
 import csv
+import matplotlib.pyplot as plt
 
 def data_from_csv(name_csv,two_channels=True,t_min=-1e20,t_max=1e20):
 	t = np.array([])
@@ -55,20 +56,74 @@ def r_sinc(t,t_pt,V_pt,T):
 		out += -V_pt[j] * k_sinc(t + t_pt[j],T)
 	return out
 
-def reconstruct(t,V,T,delay,duty):
+def reconstruct(t,V,T,delay,err):
 	t_pt = np.array([])
 	V_pt = np.array([])
 
-	#t_i = 0 - 0.0001
-	t_i = 0 + 0.00025
+	if err:
+		t_i = delay# + T/2
 
-	while t_i>min(t):
-		t_i -= duty
+		while t_i>min(t):
+			t_i -= T
+		while t_i<max(t):
+			index = np.argmin(abs(t - t_i))
+			t_pt = np.append(t_pt, t[index]-T/2)
+			V_pt = np.append(V_pt, V[index])
+			t_i += T
+	else:
+		t_i = delay
 
-	while t_i<max(t):
-		index = np.argmin(abs(t - t_i))
-		t_pt = np.append(t_pt, t[index]-delay)
-		V_pt = np.append(V_pt, V[index])
-		t_i += duty
+		while t_i>min(t):
+			t_i -= T
+		while t_i<max(t):
+			index = np.argmin(abs(t - t_i))
+			t_pt = np.append(t_pt, t[index]-T/2)
+			V_pt = np.append(V_pt, V[index])
+			t_i += T
 
 	return t_pt, V_pt, r_tr(t,t_pt,V_pt,T), r_sinc(t,t_pt,V_pt,T)
+
+def plot_figure(t,V1,V2,t_pt,r_tr,r_sinc,V_pt,t_min,t_max):
+	##rimuovi dati al di fuori dei limiti temporali
+	index_t = (t < t_min) | (t > t_max)
+	index_t_pt = (t_pt < t_min) | (t_pt > t_max)
+	V1 = np.delete(V1, index_t)
+	V2 = np.delete(V2, index_t)
+	r_tr = np.delete(r_tr, index_t)
+	r_sinc = np.delete(r_sinc, index_t)
+	t = np.delete(t, index_t)
+	V_pt = np.delete(V_pt, index_t_pt)
+	t_pt = np.delete(t_pt, index_t_pt)
+
+	##crea nuova figura
+	fig = plt.figure()
+	ax0,ax1 = fig.subplots(2,1)
+
+	# ax0.axis([-0.002, 0.002, -.5, .5])
+	# ax1.axis([-0.020, -0.016, -.5, .5])
+
+	ax0.plot(t,V1, 'b-', label='Segnale', linewidth=1)
+	ax0.plot(t,V2, 'g-', label='Campionamento')
+	ax0.plot(t_pt,V_pt, 'k+', label='Punti campionati')
+
+	ax1.plot(t,V1, 'b-', label='Segnale', linewidth=1)
+	ax1.plot(t,V2, 'g-', label='Campionamento')
+	ax1.plot(t_pt,V_pt, 'k+', label='Punti campionati')
+
+	ax0.plot(t, r_tr, 'r-', linewidth=0.7)
+	ax1.plot(t, r_sinc, 'r-', linewidth=0.7)
+
+	ax1.set_xlabel('t [s]')
+	ax0.set_ylabel('V [V]')
+	ax1.set_ylabel('V [V]')
+	#ax0.legend()
+
+	ax0.minorticks_on()
+	ax0.grid(b=True, which='major', color='#d3d3d3', linestyle='-')
+	ax0.grid(b=True, which='minor', color='#d3d3d3', linestyle=':')
+
+	ax1.minorticks_on()
+	ax1.grid(b=True, which='major', color='#d3d3d3', linestyle='-')
+	ax1.grid(b=True, which='minor', color='#d3d3d3', linestyle=':')
+	fig.tight_layout()
+	plt.show()
