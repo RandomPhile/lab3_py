a2ab4888fd346cc74fcb981e87379fc6de7680a0
RandomPhile <richifurlan@gmail.com>
2021-11-03 23:31:32 +0100

    min max

--- esp6/reconstruction.py
+++ esp6/reconstruction.py
@@ -9,26 +9,39 @@ colori = ['b','g','r','c','m','y','k']
 #t,V1,V2 = data_from_csv('reconstructionExample/sine_100_Hz')
 
 parametri = [
-{'name': 'reconstructionExample/sine_100_Hz', 'T': 0.001, 'delay': 0.0005, 't_offset': -0.0001, 'alias': False},
-{'name': 'misure2/scope_0', 'T': 1/1005.3, 'delay': 0.00055, 't_offset': 0.00040, 'alias': False},
-{'name': 'misure2/scope_1', 'T': 1/1005.3, 'delay': 0.00055, 't_offset': 0.00057, 'alias': False},
-{'name': 'misure2/scope_2', 'T': 1/1005.3, 'delay': 0.00055, 't_offset': 0.00017, 'alias': False},
-{'name': 'misure2/scope_3', 'T': 1/1005.3, 'delay':-0.0005, 't_offset': -0.0006, 'alias': True},
-{'name': 'misure2/scope_4', 'T': 1/1005.3, 'delay': 0.00055, 't_offset': 0.00052, 'alias': True},
-{'name': 'misure2/scope_5', 'T': 1/1005.3, 'delay': 0.00055, 't_offset': 0.00002, 'alias': True},
-{'name': 'misure2/scope_6', 'T': 1/1005.3, 'delay': 0.00055, 't_offset': 0.00024, 'alias': True},
-{'name': 'misure2/scope_7', 'T': 1/1005.3, 'delay': 0.00055, 't_offset': 0.00051, 'alias': True},
+{'name': 'reconstructionExample/sine_100_Hz', 'T': 0.001, 'delay': -0.0001, 'alias': False},
+{'name': 'misure2/scope_0', 'T': 1/1005.3, 'delay': 0.00033, 'alias': False},
+{'name': 'misure2/scope_1', 'T': 1/1005.3, 'delay': 0.00052, 'alias': False},
+{'name': 'misure2/scope_2', 'T': 1/1005.3, 'delay': 0.00011, 'alias': False},
+{'name': 'misure2/scope_3', 'T': 1/1005.3, 'delay': -0.00012, 'alias': True},
+{'name': 'misure2/scope_4', 'T': 1/1005.3, 'delay': 0.00052, 'alias': True},
+{'name': 'misure2/scope_5', 'T': 1/1005.3, 'delay': 0.00002, 'alias': True},
+{'name': 'misure2/scope_6', 'T': 1/1005.3, 'delay': 0.00024, 'alias': True},
+{'name': 'misure2/scope_7', 'T': 1/1005.3, 'delay': 0.00051, 'alias': True},
 ]
 
-n = 3
+n = 4
 name = parametri[n]['name']
 T = parametri[n]['T']
 delay = parametri[n]['delay']
-t_offset = parametri[n]['t_offset']
 alias = parametri[n]['alias']
 
-t,V1,V2 = data_from_csv(name,True,-0.02,0.02)
-t_pt,V_pt,r_tr,r_sinc = reconstruct(t,V2,T,delay,t_offset,alias)
+t,V1,V2 = data_from_csv(name,True)
+t_pt,V_pt,r_tr,r_sinc = reconstruct(t,V2,T,delay,alias)
+
+t_min, t_max = -0.02,0.02
+
+index_t = (t < t_min) | (t > t_max)
+index_t_pt = (t_pt < t_min) | (t_pt > t_max)
+V1 = np.delete(V1, index_t)
+V2 = np.delete(V2, index_t)
+r_tr = np.delete(r_tr, index_t)
+r_sinc = np.delete(r_sinc, index_t)
+t = np.delete(t, index_t)
+V_pt = np.delete(V_pt, index_t_pt)
+t_pt = np.delete(t_pt, index_t_pt)
+
+
 
 fig = plt.figure()
 ax0,ax1 = fig.subplots(2,1)
@@ -44,8 +57,11 @@ ax1.plot(t_pt,V_pt, 'k+', label='Punti campionati')
 ax0.plot(t, r_tr, 'r-', linewidth=0.7)
 ax1.plot(t, r_sinc, 'r-', linewidth=0.7)
 
-#ax0.axis([-0.002, 0.002, -1, 1])
-#ax1.axis([-0.020, -0.016, -1, 1])
+# ax0.set_xlim([-0.02, 0.02])
+# ax1.set_xlim([-0.02, 0.02])
+
+# ax0.axis([-0.002, 0.002, -1, 1])
+# ax1.axis([-0.020, -0.016, -1, 1])
 
 ax1.set_xlabel('t [s]')
 ax0.set_ylabel('V [V]')
@@ -59,4 +75,5 @@ ax0.grid(b=True, which='minor', color='#d3d3d3', linestyle=':')
 ax1.minorticks_on()
 ax1.grid(b=True, which='major', color='#d3d3d3', linestyle='-')
 ax1.grid(b=True, which='minor', color='#d3d3d3', linestyle=':')
+fig.tight_layout()
 plt.show()
