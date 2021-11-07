02bb7ee09ab3fe7fe63a5f3585c13ec11e707180
RandomPhile <richifurlan@gmail.com>
2021-11-07 19:31:19 +0100

    ciao Ado

--- esp5/esp5_3.py
+++ esp5/esp5_3.py
@@ -8,39 +8,61 @@ from tabulate import tabulate
 from texttable import Texttable
 import latextable
 
-R1 = np.array([10e3, 1e3 , 10e3])
+R, C = 100e3, 100e-9
+tau = R*C
+
+R1 = np.array([1e3, 10e3, 10e3])
 R2 = np.array([10e3, 10e3, 1e3 ])
 
-rapporto = R1/R2
-print(rapporto)
+r = R1/(R2+R1)
+print(r)
 
-t_discesa = np.array([26.2, 28.9, 29])*1e-6
+logr = np.log((1+r)/(1-r))
+r_mod = np.linspace(min(r), max(r))
+print(r_mod)
+logr_mod = np.log((1+r_mod)/(1-r_mod))
+T_mod = 2*tau*logr_mod
 
-T   = np.array([22.1, 3.81, 61.1])*1e-3
+t_discesa = np.array([28.9, 26.2, 29])*1e-6
+
+T   = np.array([3.81, 22.1, 61.1])*1e-3
 f = 2*pi/T
-Vpp = np.array([21.4, 21.19, 20.94])
+Vpp = np.array([21.19, 21.4, 20.94])
 
 fig, ax0 = plt.subplots()
-ax0.plot(rapporto, Vpp, 'b*', label='ampiezza')
-ax1 = ax0.twinx()
+#ax1 = ax0.twinx()
 
-ax1.plot(rapporto, T, 'r*', label='periodo')
+ax0.plot(r, T, 'r*', label='periodo')
+ax0.plot(r_mod, T_mod, 'b-', label='modello')
 
-ax1.legend()
+f = lambda r: np.log((1+r)/(1-r))
+f_inverse = lambda t: (np.exp(t)-1)/(np.exp(t)+1)
 
-print(log(21))
-#plt.show()
+ax0.set_xscale('function', functions=(f, f_inverse))
+x_labels = np.linspace(0.1, 0.9, 10)
+print(x_labels)
+ax0.set_xticks(x_labels)
+ax0.set_xticklabels(x_labels)
+
+ax0.minorticks_on()
+ax0.grid(b=True, which='major', color='#d3d3d3', linestyle='-')
+ax0.grid(b=True, which='minor', color='#d3d3d3', linestyle=':')
+
+ax0.legend()
+
+plt.show()
 
 
-rows = [['R1','R2','r']]
+rows = [['R1','R2','r','f']]
 for i in [0,1,2]:
-	rows.append([1,2,3])
+	rows.append([R1[i],R2[i],r[i],f[i]])
+
 
 table = Texttable()
-table.set_cols_align(['c'] * 3)
+table.set_cols_align(['c'] * 4)
 table.set_deco(Texttable.HEADER | Texttable.VLINES)
 table.add_rows(rows)
 
 print(tabulate(rows, headers='firstrow'))
-print('\nTabulate Latex:')
+
 print(tabulate(rows, headers='firstrow', tablefmt='latex'))
