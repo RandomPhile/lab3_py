cd1984ba33216de3c1602f73347445b8087c4542
RandomPhile <richifurlan@gmail.com>
2021-11-08 18:29:31 +0100

    si vedono i contributi di 3w_s

--- esp5/esp5_2.py
+++ esp5/esp5_2.py
@@ -11,36 +11,35 @@ f        160Hz
 '''
 #A = 1.635/2#V
 A = 1.48/2#V
-f = 160.4#Hz
+f = 160#Hz
 T = 1/f
 w_s = 2*pi/T
-
+print(w_s)
 t,V = data_from_csv('dati/scope_3',False)
-t -= T/4
-V += 0.1/4
 
-def V_mod_A(t, A):
-	return A*np.cos(w_s*t)
+def V_fit0(t, A, phi_A, C, w_s):
+	return A * np.cos(1000*w_s*t + phi_A) + C
 
-A_fit,c = curve_fit(V_mod_A, t, V)
-print(A_fit[0]*2)
-
-def V_mod(t, mu):
-	return A*( np.cos(w_s*t) - (mu/(4*w_s))*np.sin(3*w_s*t) )
+def V_fit(t, A, phi_A, B, phi_B, C):
+	return A * np.cos(w_s*t + phi_A) + B * np.cos(3*w_s*t + phi_B) + C
 
 fig1 = plt.figure()
 ax0 = fig1.subplots()
+
+fit_out0, c0 = curve_fit(V_fit0, t, V)
+fit_out, c = curve_fit(V_fit, t, V)
+
+print(fit_out0,fit_out)
+
+V_mod0 = V_fit0(t, *fit_out0)
+V_mod = V_fit(t, *fit_out)
 ax0.plot(t,V, 'b-', label=r'$V$', linewidth=1)
+ax0.plot(t, V_mod0, 'r-', label=r'Fit', linewidth=1)
+ax0.plot(t, (V-V_mod0)*100, 'g-', label=r'residui*50', linewidth=1)
 
-popt, pcov = curve_fit(V_mod, t, V)
 
-V_mod0 = V_mod_A(t, A_fit)
-V_mod1 = V_mod(t, popt[0])
-
-ax0.plot(t, (V-V_mod0)*100, 'k-', label=r'$\mu=0$', linewidth=1)
-
-ax0.plot(t, V_mod_A(t, A_fit), 'k-', label=r'$\mu=0$', linewidth=1)
-#ax0.plot(t, V_mod(t, *popt), 'r-', label=r'Fit: $\mu=%5.2f$' % tuple(popt), linewidth=1)
+# ax0.plot(t, V_mod_A(t, A_fit), 'k-', label=r'$\mu=0$', linewidth=1)
+# #ax0.plot(t, V_mod(t, *popt), 'r-', label=r'Fit: $\mu=%5.2f$' % tuple(popt), linewidth=1)
 
 ax0.set_xlabel('t [s]')
 ax0.set_ylabel('V [V]')
