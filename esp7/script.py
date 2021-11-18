de5e331e7f5fdeba734bfb89d52199d8ca883ac9
RandomPhile <richifurlan@gmail.com>
2021-11-18 16:15:35 +0100

    siunitx non funziona ancora

--- esp7/script.py
+++ esp7/script.py
@@ -1,35 +1,50 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib as mpl
 from functions import *
 from base import *
+from scipy.optimize import curve_fit
 
-def reg_lin(x, y):
-	N = len(x)
-	x_mean = np.mean(x)
-	y_mean = np.mean(y)
+if plt.rcParams["text.usetex"] is False:
+    plt.rcParams["text.usetex"] = True
+    print("\nWARNING: text.usetex is now set to True\n")
 
-	m_num = ((x - x_mean) * (y - y_mean)).sum()
-	m_den = ((x - x_mean)**2).sum()
-	m = m_num / m_den
-	
-	q = y_mean - (m*x_mean)
-
-	y_line = q + m*x
-
-	return m, q, y_line
+if "siunitx" not in plt.rcParams["text.latex.preamble"]:
+    plt.rcParams["text.latex.preamble"] += (r"\usepackage{siunitx}")
+print(plt.rcParams["text.latex.preamble"])
 
 #N = np.arange(1,13)
 N =          np.array([1,    2,    3,    4,    5,    6,    7,    8,    9,    10,   11,    12])
-t_diff =     np.array([58.0, 60.4, 66.0, 69.8, 74.6, 79.2, 80.0, 83.2, 88.0, 93.0, 96.6,  101.6])*1e-9
-t_diff_max = np.array([59.6, 60.8, 67.8, 71.1, 76.8, 79.6, 84.8, 83.8, 88.4, 93.6, 97.4,  102.0])*1e-9
-dt_diff = np.abs(t_diff_max-t_diff)/2
+t =     np.array([58.0, 60.4, 66.0, 69.8, 74.6, 79.2, 80.0, 83.2, 88.0, 93.0, 96.6,  101.6])*1e-9
+t_max = np.array([59.6, 60.8, 67.8, 71.1, 76.8, 79.6, 84.8, 83.8, 88.4, 93.6, 97.4,  102.0])*1e-9
+dt = np.abs(t_max-t)/2
+
+
+t_mod = lambda x,m,q : m*x+q
+t_mod0 = lambda x,m : m*x
+
+popt, pcov = curve_fit(t_mod, N, t, sigma=dt)
+#m0, pcov0 = curve_fit(t_mod0, N, t, sigma=dt)
+
+m, q = popt
+dm, dq = np.sqrt(np.diag(pcov))
+#dm0= np.sqrt(np.diag(pcov0))
+
 
 fig = plt.figure()
 ax = fig.subplots()
-ax.errorbar(N, t_diff, yerr=dt_diff, fmt=' ', color='b', marker='o', ms=3)
-m,q,t_diff_fit = reg_lin(N, t_diff)
-ax.plot(N, t_diff_fit)
-print(m,q)
+ax.errorbar(N, t*1e9, yerr=dt*1e9, fmt=' ', color='b', marker='o', ms=2, label='Dati')
+#ax.plot(N,t_mod(N,m,q)*1e9, label=r'Fit lineare: $m=(4.0\pm0.1\qty{9.8}{kg.m.s^{-2}})$ns')
+#ax.plot(N,t_mod(N,m,q)*1e9, label=r'Fit lineare: $\qty{9.8}{kg.m.s^{-2}}$')
+plt.title(r"Disctances given in \si{\metre}")
+#ax.plot(N,t_mod0(N,m0))
 
+print('m = ',m,'+-',dm)
+
+#ax.set_xlabel(r'$N$: # di porte logiche')
+ax.set_ylabel(r'Ritardo $t_r$ [ns]')
+
+ax.legend()
 grid(ax)
-fig.show()
+fig.savefig('fig/fit.eps', format='eps')
+#fig.show()
